918917821d47db3b6e09d570e8d93431e0de4d7f
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-05-04 16:15:14 +0530

    smoothened movements of pointer. Projectile releases according to pointer

---
+++ Assets/Scripts/pointer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointer : MonoBehaviour
+{
+    private Rigidbody2D myBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool keyPressed = true;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = transform.position - new Vector3(1, 0, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = transform.position + new Vector3(1, 0, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = transform.position + new Vector3(0, 1, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = transform.position - new Vector3(0, 1, 0);
+        }
+        else keyPressed = false;
+
+        if (keyPressed) {
+            StartCoroutine(waitNow());
+        }
+        
+    }
+    IEnumerator waitNow() {
+        yield return new WaitForSeconds(0.1f);
+        myBody.velocity= new Vector2( 0, 0);
+    }
+    
+}
--- Assets/Scripts/projectile.cs
+++ Assets/Scripts/projectile.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class projectile : MonoBehaviour
 {
-    public float speed;             //Floating point variable to store the player's movement speed.
+                 //Floating point variable to store the player's movement speed.
 
     private Rigidbody2D rigidBody;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private CircleCollider2D mycollider;
