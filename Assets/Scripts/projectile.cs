6bcfd9518c54118cdd1af647185c2f8533be179e
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-05-04 12:43:50 +0530

    added script for projectile

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(0, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
