bb493567d01a7aa3c65df430693eed0e7cd40620
Priyanshu <xnkasy@Priyanshus-MacBook-Pro.local>
2019-05-05 10:51:07 +0530

    commented code

--- Assets/Scripts/projectile.cs
+++ Assets/Scripts/projectile.cs
@@ -9,6 +9,8 @@ public class projectile : MonoBehaviour
     private Rigidbody2D rigidBody;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
     private CircleCollider2D mycollider;
     private GameObject pointer;
+    Vector2 zero_vector;
+    Vector2 vector_temp;
     // Use this for initialization
     void Start()
     {
@@ -16,6 +18,7 @@ public class projectile : MonoBehaviour
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.bodyType = RigidbodyType2D.Kinematic;
         pointer = GameObject.Find("pointer");
+        zero_vector = Vector2.zero;
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
@@ -33,6 +36,39 @@ public class projectile : MonoBehaviour
             }
 
         }
+
     }
+    /* if (Input.touchCount > 0 &&  rigidBody.isKinematic && pointer != null)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Handle finger movements based on touch phase.
+            switch (touch.phase)
+            {
+                // Record initial touch position.
+                case TouchPhase.Began:
+                    TouchTime = Time.time;​
+                    break;
+
+                // Determine direction by comparing the current touch position with the initial one.
+                case TouchPhase.Moved:
+                   
+                    break;
+
+                // Report that a direction has been chosen when the finger is lifted.
+                case TouchPhase.Ended:
+                    float velocity=Time.time-TouchTime;              
+                    vector_temp = pointer.transform.position - this.transform.position;
+                    rigidBody.bodyType = RigidbodyType2D.Kinematic;
+                    float angle=Vector2.angle(vector_temp, zero_vector);
+                    Vector2 new_vel= (velocity*cos(angle),velocity*sin(angle));
+                    rigidBody.velocity=new_vel;                   
+                    rigidBody.isKinematic = false;
+                    Destroy(pointer);                   
+                    break;
+            }
+        }
+
+         */
 
 }
