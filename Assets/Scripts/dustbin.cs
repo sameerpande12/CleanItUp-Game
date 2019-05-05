4f7a7fd07e892afde59d2a0686cc6016fa4db028
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-05-05 19:48:40 +0530

    added a scoring mechanism

--- Assets/Scripts/dustbin.cs
+++ Assets/Scripts/dustbin.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class dustbin : MonoBehaviour
 {
     // Start is called before the first frame update
-
-
+    private score scoreScript;
+    private GameObject scoreObject;
+     void Start()
+    { scoreObject = GameObject.Find("ScoreKeeper");
+      scoreScript = scoreObject.GetComponent<score>() ;
+    }
     // Update is called once per frame
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameObject other = collision.gameObject;
+        
         if (other.tag == "Projectile")
         {
+            scoreScript.points = scoreScript.points + 5;
             Destroy(other);
         }
 
--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-public class _SceneMangement : MonoBehaviour
+
+public class score : MonoBehaviour
 {
+    public  int points = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        points = 0;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
