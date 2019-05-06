9cbb31c7d01377774b8dc8486debf55502fea670
xnkasy <xnkasypriyanshu@gmail.com>
2019-05-06 06:06:45 +0530

    Scene Change is working

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(string level)
+    {
+        SceneManager.LoadScene(level, LoadSceneMode.Single);
+    }
+}
