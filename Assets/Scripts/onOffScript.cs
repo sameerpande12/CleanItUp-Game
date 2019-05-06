9df0365c3cdfeca1817d8748aad537bc295b1397
Sameer Pande <Sameer.Vivek.Pande.cs117@cse.iitd.ac.in>
2019-05-07 02:58:37 +0530

    added prevLevel feature

--- Assets/Scripts/onOffScript.cs
+++ Assets/Scripts/onOffScript.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class onOffScript : MonoBehaviour
 {
     [SerializeField] private Button pauseButton;
     [SerializeField] private Button resumeButton;
     [SerializeField] private Button nextLevelButton;
+    [SerializeField] private Button previousLevelButton;
     [SerializeField] private GameObject pauseMenuUI;
-
+    
     //private bool isPaused = false;
     
     // Start is called before the first frame update
@@ -17,7 +18,7 @@ public class onOffScript : MonoBehaviour
     {
         pauseButton.onClick.AddListener(pauseGame);
         resumeButton.onClick.AddListener(resumeGame);
-        nextLevelButton.onClick.AddListener(changeLevel);
+        nextLevelButton.onClick.AddListener(nextLevel);
         pauseMenuUI.SetActive(false);
     }
 
@@ -35,9 +36,52 @@ public class onOffScript : MonoBehaviour
         pauseMenuUI.SetActive(false);
 
     }
-    void changeLevel()
-    {
+    void nextLevel()
+    { string sceneName = SceneManager.GetActiveScene().name;
+        if(sceneName == "Level1")
+        {
+            SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+        }
+        else if(sceneName == "Level2")
+        {
+            SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level3")
+        {
+            SceneManager.LoadScene("Level4", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level4")
+        {
+            SceneManager.LoadScene("Level5", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level5")
+        {
+            SceneManager.LoadScene("LevelSelector", LoadSceneMode.Single);
+        }
+    }
 
+    void previousLevel() {
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName == "Level1")
+        {
+            SceneManager.LoadScene("LevelSelector", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level2")
+        {
+            SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level3")
+        {
+            SceneManager.LoadScene("Level2", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level4")
+        {
+            SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+        }
+        else if (sceneName == "Level5")
+        {
+            SceneManager.LoadScene("Level4", LoadSceneMode.Single);
+        }
     }
     
     
